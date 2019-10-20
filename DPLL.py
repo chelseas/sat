634a293c15e61a6da59a05893b2b7d5379a5e1c9
Chelsea Sidrane <csidrane@stanford.edu>
2019-10-19 18:20:59 -0700

    DPLL

--- DPLL.py
+++ DPLL.py
@@ -3,27 +3,66 @@ from enum import Enum
 class status(Enum):
     SAT = 1
     UNSAT = 0
+    WORKING = 85
 
 class DPLL():
     def __init__(self):
-        pass
+        self.model = []  # list of (var, bool) tuples
+        self.decisions = []  # list of var (integers)
 
     def run_solver(self, file):
-        self.parse_input(file)
+        self.parse_input(file) # generates self.formula
         self.solve()
 
     def solve(self):
-        pass
+        SATus = status.WORKING
+        funs = [self.apply_pure_literal,
+                self.apply_unit_prop,
+                self.decide]
+        counter = 0
+        while SATus is status.WORKING:
+            if counter % 3 == 0:
+                funs[0]()
+            elif counter % 3 == 1:
+                funs[1]()
+            elif counter % 3 == 2:
+                funs[2]()
+            counter +=1
+            SATus = self.check_SATus() # backtracking is in here
 
     def check_SATus(self):
-        if len(self.formula) == 0:
-            return status.SAT
-        elif min([len(clause) for clause in self.formula]) == 0:
-            return status.UNSAT
-        else:
-            return "in progress..."
+        # check for SAT or UNSAT
+        # if pseudofail, backtrack and continue
+        # if real fal, report SATus as UNSAT
+        pass
 
+    def apply_pure_literal(self):
+        pass
+
+    def backtrack(self):
+        pass
 
     def parse_input(self, file):
-        # stuff to read file
-        self.formula = None # replace with stuff. list of clauses
+        farray = open(file).read().split('\n')
+        # get rid of comments
+        # assume no whitespace at top of file
+        # just:
+        # c
+        # c
+        # p cnf .. ..
+        # 1 3 -5 0
+        # ...
+        for i in range(len(farray)):
+            if farray[i][0] is not 'c':
+                assert(farray[i][0] is 'p')
+                infos = farray[i].split()
+                self.nvars = int(infos[-2])
+                self.nclauses = int(infos[-1])
+                break
+        formula = [self.stripper(clause) for clause in farray[i+1:]]
+        # strip empty clauses
+        formula = [clause for clause in formula if len(clause)>0]
+        self.formula = formula
+
+    def stripper(self, stringtuple):
+        return [int(x) for x in stringtuple.split()][:-1] # drop final 0
---
+++ testing_DPLL.py
@@ -0,0 +1,4 @@
+from DPLL import DPLL
+
+solver = DPLL()
+solver.parse_input("/Users/Chelsea/scratch/n_queens_3.txt")
